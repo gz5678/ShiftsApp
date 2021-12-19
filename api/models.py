a9556fcef195c36e9355111e97428e0941a20797
gz5678 <gil.zivor@mail.huji.ac.il>
2021-12-19 21:46:55 +0100

    Removed team lead column for shiftsuser model since it's implicit in the group table. Added test_data fixture for adding mock data for testing

--- api/models.py
+++ api/models.py
@@ -4,6 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class ShiftsUser(User):
-
     is_team_lead = models.BooleanField(verbose_name="is_team_lead", default=False)
-    team_lead = models.CharField(verbose_name='Team Lead Name',max_length=150)
